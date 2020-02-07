7e778bb3a1c2b25b2aab109326b81d1afafc880b
Chandler Prince <david.prince@wust.edu>
2020-02-07 07:51:00 -0600

    Sending to github so I can get feedback

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.8.1-slim
 RUN pip install --no-cache-dir numpy==1.18.1
 
 ADD test.sh /
-RUN chmod 775 /test.sh
+RUN chown 775 /test.sh
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-_is_numpy1.18.1_installed="python -c \"import numpy; print(numpy.version.version)\" | grep 1.8.1"
-if [ _is_numpy1.18.1_installed ==  0]; then
+_is_numpy1.18.1_installed=$(python -c "import numpy; print(numpy.version.version)" | grep 1.8.1 | wc -l)
+if [[ $_is_numpy1.18.1_installed = 1 ]]; then
     echo "Correct Version of Numpy installed"
 else
     echo "Wrong Numpy Version"
